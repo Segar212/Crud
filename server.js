f87fdcd270e1029a2f29436f1937015fba3920ae
Ben Angek <ben@Bens-MacBook-Air.local>
2018-02-13 16:35:37 -0700

    finished

--- server.js
+++ server.js
@@ -1,1 +1,83 @@
-node_modules
+//base setup
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/bears');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var Bear = require('./public/bear');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+var port = process.env.port || 8080;
+
+var router = express.Router();
+router.use(function(req, res, next) {
+  console.log('Something is happening.');
+  next();
+});
+//routes for our api
+router.route('/bears')
+  .post(function(req, res) {
+    var bear = new Bear();
+    bear.name = req.body.name;
+    bear.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({
+        message: 'Bear created!'
+      });
+    });
+  })
+  .get(function(req, res) {
+    Bear.find(function(err, bears) {
+      if (err)
+        res.send(err);
+      res.json(bears);
+    });
+  });
+router.get('/', function(req, res) {
+  res.json({
+    message: 'hooray! welcom to out api!'
+  });
+});
+router.route('/bears/:bear_id')
+  .get(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+      res.json(bear);
+    });
+  })
+  .put(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+      bear.name = req.body.name;
+      bear.save(function(err) {
+        if (err)
+          res.send(err);
+        res.json({
+          message: 'Bear updated!'
+        });
+      });
+    });
+  })
+  .delete(function(req, res) {
+    Bear.remove({
+      _id: req.params.bear_id
+    }, function(err, bear) {
+      if (err)
+        res.send(err);
+      res.json({
+        message: 'Successfully deleted'
+      });
+    });
+  });
+app.use('/api', router);
+
+app.listen(port);
+
+console.log('Magic happens on port' + port);
